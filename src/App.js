dbd0721f7bb8cf4c2cc27d607cd33f939621d020
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-02 21:23:14 +0700

    feat: update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Alert, Button, Container } from 'reactstrap';
+import styles from './style.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { danger: false };
+
+  handleOnClick = event => {
+    this.setState({danger: true });
+  }
+  render(){
+    const{danger}=this.state
+    return(
+      <Container className={styles.container}>
+        <Container className="text-center">
+          <Button className="mb-4" color="danger" onClick={this.handleOnClick}>Hati-Hati!</Button>
+          {danger && <Alert color="danger">Sudah dibilang hati-hati!</Alert>}
+        </Container>
+      </Container>
+    )
+  }
 }
 
 export default App;
