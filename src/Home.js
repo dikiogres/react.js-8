a6c978cbf3e03a88686f48e8284ec5963e878d2b
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-03 23:30:35 +0700

    feat: implement fetch methods on Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,29 @@ import React, { Component, Fragment } from 'react';
 import {Container} from 'reactstrap';
 import Navbar from '../components/Navbar';
 
-class Home extends Component{
+class Post extends Component{
     render(){
+        const{title}=this.props 
+        return <h5>{title}</h5>
+    }
+}
+
+class Home extends Component{
+    state = {posts: [] }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response=>response.json())
+        .then(result=>{this.setState({posts: result})
+        })
+    }
+    render(){
+        const { posts } = this.state
         return(
             <Fragment>
                 <Navbar/>
-                <Container className="p-4"><h1>Ini Halaman Home</h1></Container>
+                <Container className="p-4">
+                    {posts.map(i=> <Post title={i.title} />)}
+                </Container>
             </Fragment>
         )
     }
